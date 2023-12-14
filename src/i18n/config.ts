5b86b8e9030fe93d01c6baa1e4999656eefd66e9
Jonathan.Vega <Ing.JonathanV@outlook.com>
2023-12-14 14:18:18 -0500

    fix build error

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,4 +1,4 @@
 export const defaultLocale = 'es'
-export const locales = [defaultLocale, 'es'] as const
+export const locales = [defaultLocale, 'en'] as const
 export type Locales = (typeof locales)[number]
 export const cookieName = 'i18n-locale'
