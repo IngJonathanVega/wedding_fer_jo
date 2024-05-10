7afa1c36a4c518d9c1360822587cceeda9c7ad07
Jonathan.Vega <Ing.JonathanV@outlook.com>
2024-05-10 07:16:15 -0400

    build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,6 @@ export const metadata: Metadata = {
   description: 'Wedding for Fernanda & Jonathan',
 };
 
-
-
 const LocaleRootLayout: FC<PropsWithChildren<PageProps>> = async ({ children, params }) => {
   return (
     <html lang={params.locale}>
